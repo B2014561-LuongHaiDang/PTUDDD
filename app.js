98fe6919b49820c58a0d807b914981f98e77dbd1
B2014561_LuongHaiDang <dangb2014561@student.ctu.edu.vn>
2024-01-23 20:07:15 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 
 const contactsRouter = require("./app/routes/contact.route");
 
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -11,6 +13,16 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+    });
+});
 
 app.get("/", (req, res) =>{
     res.json({message: "Welcomed to contact book application."});
